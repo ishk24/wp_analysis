5c4e3ff15bfc45bc1e0162da8979824adf8dea88
ishk24 <bhagtaniishika22@gmail.com>
2024-09-13 18:09:36 +0530

    bunf

---
+++ demo.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import preprocessor,helper
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+st.sidebar.title("Whatsapp Chat Analyzer")
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode("utf-8")
+    df = preprocessor.preprocess(data)
